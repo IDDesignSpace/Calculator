c2d905ff379072c8c0409c9a5180329cc9db75a3
root <ibarkan117@gmail.com>
2023-10-17 17:16:47 -0400

    updated UI

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Get input
+// Get all number buttons
+// Get all math operator buttons
+// Create a function that displays input from buttons on
+// Create a function that handles math operations
+// Attach eventhandlers
+
+const calculatorInput = document.querySelector("#calculator-input");
+const numberBtns = document.querySelectorAll(".numbers");
+const mathOperatorBtns = document.querySelectorAll(".math_operators");
+
+const displayClick = function () {};
+
+const calcInput = function (input) {
+  // Store all input in an array
+  // Convert all numbers to numbers
+  // Calculate input
+  // return
+};
