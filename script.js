3d8ec3a400f3bdd024d66394f543587acb3eb9bf
Ian Barkan <42154502+IDDesignSpace@users.noreply.github.com>
2023-10-17 23:46:10 -0400

    created class structure and got key board input working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Get input
 // Get all number buttons
@@ -7,15 +7,64 @@
 // Create a function that handles math operations
 // Attach eventhandlers
 
-const calculatorInput = document.querySelector("#calculator-input");
-const numberBtns = document.querySelectorAll(".numbers");
-const mathOperatorBtns = document.querySelectorAll(".math_operators");
+const calculatorInput = document.querySelector('#calculator-input');
+// const numberBtns = document.querySelectorAll('.numbers');
+// const mathOperatorBtns = document.querySelectorAll('.math_operators');
+// const numberBtns = document.querySelectorAll('.numbers');
 
-const displayClick = function () {};
-
-const calcInput = function (input) {
+class Calculation {
   // Store all input in an array
-  // Convert all numbers to numbers
+  // Convert all number strings to numbers
+  // Convert all symbols to math operators;
   // Calculate input
   // return
-};
+  input;
+  sum;
+
+  constructor(input) {
+    this.input = input;
+    this.calculate();
+    this.returnSum();
+  }
+
+  calculate() {
+    this.sum = eval(this.input);
+  }
+  returnSum() {
+    console.log(this.sum);
+    return this.sum;
+  }
+}
+
+class Calculator {
+  input;
+  returnSum;
+
+  constructor() {
+    // Handle enter key
+    calculatorInput.addEventListener('keypress', this.returnKey.bind(this));
+  }
+
+  updateUI() {
+    console.log(this.returnSum);
+    calculatorInput.value = this.returnSum;
+  }
+  returnKey(keypressed) {
+    // If the key pressed is the enter key grab input value
+    if (keypressed.keyCode === 13) this.filterInput(calculatorInput.value);
+  }
+  filterInput(calcInput) {
+    // This will check to see if there are any characters that shouldnt be included
+    // If there are characters that arent in the calculator
+    // This will also convert characters from html codes to math operators
+    // It will then return an array of characters joined in a string to be called by newCalculation
+    this.newCalculation(calcInput);
+  }
+  newCalculation(filteredInput) {
+    console.log(filteredInput);
+    this.returnSum = new Calculation(filteredInput).returnSum();
+    this.updateUI();
+  }
+}
+
+const newCalculator = new Calculator();
